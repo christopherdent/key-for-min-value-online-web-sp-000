119f20a2412a2ac691e75a118c1bcf54ba46b8d3
Christopher Dent <christopherdent01@gmail.com>
2019-12-26 20:11:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,40 +7,15 @@
 #* Think about how to collect or store the correct key. Remember that you need your method to return *just this key*. 
 
 require 'pry'
-def key_for_min_value(name_hash)
 
-lowest = 10000 
-<<<<<<< HEAD
-key = nil 
-=======
->>>>>>> 45ed7a03d245f44be81cea8ba6d96ad8e2d4c30c
 
-  if name_hash == {}
-    return nil 
- 
-  else
-    name_hash.collect do |name, value|
-<<<<<<< HEAD
-      if value < lowest
-        lowest = value
-        key = name 
-       
-      end 
-      
-     # binding.pry
-=======
-      
-      
-      
-      binding.pry
->>>>>>> 45ed7a03d245f44be81cea8ba6d96ad8e2d4c30c
-      
-    end  
+
+def key_for_min_value(hash)
   
+  if hash.empty?
+    return nil 
   end 
-<<<<<<< HEAD
-key
-=======
-
->>>>>>> 45ed7a03d245f44be81cea8ba6d96ad8e2d4c30c
-end
+  
+  
+  
+end 
